d6092888d610bf7680d021d25e4ef9e72469d659
rikyriver <78733299+rikyriver@users.noreply.github.com>
2021-02-11 10:37:15 +0200

    Update DashboardController.java

--- src/main/java/com/kaliman/dashboard/controller/DashboardController.java
+++ src/main/java/com/kaliman/dashboard/controller/DashboardController.java
@@ -1,37 +1,7 @@
 package com.kaliman.dashboard.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-import com.kaliman.dashboard.repository.ChartInfo;
 
 @Controller
 public class DashboardController {
-
-	@RequestMapping(value = "/usersBetsTimeCharts1")
-	public ModelAndView displayAllUserChartBar2(HttpServletRequest request) {
- 
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("usersBetsTimeCharts1");
-		return mv;
-
-	}
-
-	@RequestMapping(value = "/userTimeChart")
-	public ModelAndView displayAllUserChartBar(HttpServletRequest request) {
-
-		// String account = request.getParameter("uname");
-		// String password = request.getParameter("psw");
-		ModelAndView mv = new ModelAndView();
-		// System.out.println("User login:" + account + "::password:" + password);
-		mv.setViewName("userTimeChart");
-		return mv;
-
-	}
-
 }
